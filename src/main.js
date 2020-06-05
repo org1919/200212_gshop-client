07879b0e58e874c729d54d4d94a402c59ce5acd0
org <chai1919@outlook.com>
2020-06-05 12:02:13 +0800

    'route'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-const a = 1
+import router from './router/index'
 
 new Vue({
   // el: "#app",
-  render: h => h(App)
+  render: h => h(App),
+  router //配置路由器 ==> 所有的组件都可以通过$router属性得到路由对象
 }).$mount("#app")
